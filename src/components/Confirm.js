4c128a2a4772a17e15c3899889f425160b1cd334
punithashunmugam4 <punithashunmugam4@gmail.com>
2022-11-28 18:21:46 +0530

    return to home

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import ReactDOM from "react-dom";
 import { availability } from "../Data";
+import {Link} from 'react-router-dom';
 
 const Confirm=({details})=>{
     const [success,setSuccess]=useState(false);
@@ -24,11 +25,12 @@ const Confirm=({details})=>{
                     <p>Contact: {details.contact}</p>
                     <p>Movie: {details.movie}</p>
                     <p>Booked Seats: {details.seats.map((seat,index)=>{
-                       if(index<details.seats.length-1) return  <span>{seat},</span>
+                       if(index<details.seats.length-1) return  <span key={seat}>{seat},</span>
                        else return <span>{seat}.</span>
                     })}</p>
                     <button className="confirm-btn" onClick={handleConfirm}>Confirm Details</button>
-         </div>:<div className="confirm-details"><h1>Hi Mr.{details.name},<br/>Seats Confirmed Successfully!!</h1></div>}
+         </div>:<div className="confirm"><h1>Hi Mr.{details.name},<br/>Seats Confirmed Successfully!!</h1>
+         <Link to='/'><button className="to-home-btn">Go to HomePage</button></Link></div>}
          <div className="close-btn" onClick={()=>{details.setFlag(false)}}>x</div></div>,document.getElementById("root-portal")
     )
 }
