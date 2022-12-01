08cbc11b8653ee8b4edcd8375274d5b4ac6b0f4d
punithashunmugam4 <punithashunmugam4@gmail.com>
2022-12-01 18:25:26 +0530

    navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import React from "react";
+import './header.css';
 
 const Header=()=>{
     return <div id="header">
-        <h1 id="head">Movie Ticket Booking App</h1>
+        <h1 id="head">Star Cinemas</h1>
+        <nav className="navbar">
+            <a href='/'>Home</a>
+            <a href='/'>About us</a>
+            <a href='/'>Contact</a>
+        </nav>
         </div>
 }
 
